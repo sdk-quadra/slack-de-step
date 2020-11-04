c71532124b048eacf6584d75e0bd865a94b00b39
sdk-quadra <sadaka556@gmail.com>
2020-11-04 15:22:42 +0900

    メッセージ内容を編集できるようにする

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -24,6 +24,31 @@ class MessagesController < ApplicationController
     push_timing.in_x_days = in_x_days
     push_timing.time = Time.parse(time)
     push_timing.save!
+  end
+
+  def edit
+    @message = Message.find(params[:id])
+    @channel = params[:channel_id]
+  end
+
+  def update
+    @message = Message.find(params[:id])
+    @message_push_timing = Message.find(params[:id]).push_timing
+
+    text = params[:message][:message]
+
+    in_x_days = params[:message][:push_timing_attributes][:in_x_days]
+    time_hour = params[:message][:push_timing_attributes]["time(4i)"]
+    time_minute = params[:message][:push_timing_attributes]["time(5i)"]
+    time_second = params[:message][:push_timing_attributes]["time(6i)"]
+    time = "#{time_hour}:#{time_minute}:#{time_second}"
+    image = params[:message][:image]
+
+    @message.update(message: text) if text.present?
+    @message.update(image: image) if image.present?
+    @message.update(image: nil) if params["aaa"] == "1"
+
+    @message_push_timing.update(in_x_days: in_x_days, time: time)
 
   end
 end
