df9fdbc8dae8f5439960a25cba611419a6037382
sdk-quadra <sadaka556@gmail.com>
2020-11-06 12:04:51 +0900

    defaultの送信時間が9時間ずれているので直す

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,7 @@ module SlackDeStep
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
   end
 end
