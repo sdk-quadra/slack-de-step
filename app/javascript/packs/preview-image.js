dc9473a74340370cbf3f73b11c3e34e7099945e3
sdk-quadra <sadaka556@gmail.com>
2020-11-26 09:19:24 +0900

    channel選択中は背景を付ける

--- app/javascript/packs/preview-image.js
+++ app/javascript/packs/preview-image.js
@@ -1,35 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
     const preview = document.getElementById('preview')
-    if (preview.src === '' || preview.src === null) {
-        document.getElementById('deleteImg').style.display = 'none'
+    if (preview){
+        if (preview.src === '' || preview.src === null) {
+            document.getElementById('deleteImg').style.display = 'none'
+        }
     }
 
     const messageImage = document.getElementById('message_image')
-    messageImage.addEventListener('change', e => {
-        const target = e.currentTarget
-        const file = target.files[0]
-        const reader  = new FileReader()
-        reader.onloadend = function() {
-            document.getElementById('preview').src = reader.result
-            document.getElementById('deleteImg').style.display = 'block'
-            document.getElementById('delete_check').checked = false
-        }
-        if (file) {
-            reader.readAsDataURL(file)
-        } else {
-            document.getElementById('preview').src = ''
-            document.getElementById('deleteImg').style.display = 'none'
-        }
-    })
+    if (messageImage) {
+        messageImage.addEventListener('change', e => {
+            const target = e.currentTarget
+            const file = target.files[0]
+            const reader = new FileReader()
+            reader.onloadend = function () {
+                document.getElementById('preview').src = reader.result
+                document.getElementById('deleteImg').style.display = 'block'
+                document.getElementById('delete_check').checked = false
+            }
+            if (file) {
+                reader.readAsDataURL(file)
+            } else {
+                document.getElementById('preview').src = ''
+                document.getElementById('deleteImg').style.display = 'none'
+            }
+        })
+    }
 
     const deleteBtn = document.getElementById('deleteImg')
-    deleteBtn.addEventListener('click', () => {
-        const result = window.confirm('本当に削除しますか？')
+    if (deleteBtn){
+        deleteBtn.addEventListener('click', () => {
+            const result = window.confirm('本当に削除しますか？')
 
-        if (result) {
-            document.getElementById('preview').src = ''
-            document.getElementById('delete_check').checked = true
-            document.getElementById('deleteImg').style.display = 'none'
-        }
-    })
+            if (result) {
+                document.getElementById('preview').src = ''
+                document.getElementById('delete_check').checked = true
+                document.getElementById('deleteImg').style.display = 'none'
+            }
+        })
+    }
 })
