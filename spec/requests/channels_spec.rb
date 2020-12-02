3f9748954db56a38cc4b082ddec9521471041a83
sdk-quadra <sadaka556@gmail.com>
2020-12-02 20:41:05 +0900

    request specを書く

---
+++ spec/requests/channels_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Messages", type: :request do
+  include CurlBuilder
+
+  it "user情報を取得できる事" do
+    file = ActiveSupport::JSON.decode(File.read("spec/fixtures/users_info.json"))
+    channel = ChannelsController.new
+    allow(channel).to receive(:curl_exec).and_return(file)
+
+    curl_exec = channel.curl_exec(base_url: "https://slack.com/api/users.info?user=U01B6RZ33L5", headers: { "Authorization": "Bearer " + ENV["OAUTH_BOT_TOKEN"] })
+
+    expect(curl_exec["ok"]).to eq true
+  end
+
+end
