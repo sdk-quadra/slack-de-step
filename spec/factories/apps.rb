4025cd1893a6a4f6252311b04bd4bfbe0daff210
sdk-quadra <sadaka556@gmail.com>
2020-12-03 16:26:46 +0900

    fixturesを一部修正

--- spec/factories/apps.rb
+++ spec/factories/apps.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :app do
     sequence(:workspace_id) { 1 }
     sequence(:oauth_bot_token) { ENV["OAUTH_BOT_TOKEN"] }
-    sequence(:bot_user_id) { "U01G0CZR6HX" }
+    sequence(:bot_user_id) { "U01B6RZ33L5" }
     sequence(:api_app_id) { "A01BNHJJ21X" }
   end
 end
