9c8d214858c5282d5fa0f20228ae86f88ab288af
sdk-quadra <sadaka556@gmail.com>
2020-12-04 17:56:32 +0900

    テーブルpossessionのカラムのdata typeを修正

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,8 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_081558) do
-
+ActiveRecord::Schema.define(version: 2020_12_04_084745) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,8 +67,8 @@ ActiveRecord::Schema.define(version: 2020_12_04_081558) do
   end
 
   create_table "possessions", force: :cascade do |t|
-    t.integer "user_id", default: 0
-    t.integer "workspace_id", default: 0
+    t.integer "user_id", null: false
+    t.integer "workspace_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -102,5 +103,4 @@ ActiveRecord::Schema.define(version: 2020_12_04_081558) do
     t.string "name", null: false
     t.string "icon_url", null: false
   end
-
 end
