79efdf099f5c6f6f25e411148107c6533d45068c
sdk-quadra <sadaka556@gmail.com>
2020-12-09 13:24:47 +0900

    workspace画面に伴うtestを一部修正

--- spec/system/channels_spec.rb
+++ spec/system/channels_spec.rb
@@ -15,11 +15,11 @@ RSpec.describe "channels", type: :system do
     @channel = FactoryBot.create(:channel, app_id: @app.id)
   end
 
-  it "channel一覧からworkspace一覧に戻る事" do
+  it "ログアウトするとログインページに戻る事" do
     page.set_rack_session(user_id: @user.id)
     visit workspace_channel_path(@workspace.id, @channel.id)
-    click_link "workspace_icon"
-    expect(page).to have_content "ワークスペースを選択"
+    click_link "≡"
+    expect(page).to have_content "相対日時でslackメッセージを送る"
   end
 
   it "channel一覧サイドバーにchannel名が表示されている事" do
