6e9902f1587654e54f258ebe1918b7fc3583e6b6
sdk-quadra <sadaka556@gmail.com>
2021-01-16 09:58:47 +0900

    テスト変更_7

--- spec/system/messages_spec.rb
+++ spec/system/messages_spec.rb
@@ -197,7 +197,7 @@ RSpec.describe "messages", type: :system do
         minutes = Time.now.since(5400).strftime("%M")
 
         find("#message_push_timing_attributes_in_x_days").set("1")
-        find("#message_push_timing_attributes_time").set("#{hour}:#{minutes} AM")
+        find("#message_push_timing_attributes_time").set("#{hour}:#{minutes}AM")
         find(".message-form__delete-img").click
 
         click_button "削除する"
@@ -228,7 +228,7 @@ RSpec.describe "messages", type: :system do
         minutes = Time.now.since(5400).strftime("%M")
 
         find("#message_push_timing_attributes_in_x_days").set("1")
-        find("#message_push_timing_attributes_time").set("#{hour}:#{minutes} AM")
+        find("#message_push_timing_attributes_time").set("#{hour}:#{minutes}AM")
         click_button "テスト送信"
 
         expect(find("#overlay-test-submit", visible: true)).to be_visible
@@ -242,7 +242,7 @@ RSpec.describe "messages", type: :system do
         minutes = Time.now.since(5400).strftime("%M")
 
         find("#message_push_timing_attributes_in_x_days").set("1")
-        find("#message_push_timing_attributes_time").set("#{hour}:#{minutes} AM")
+        find("#message_push_timing_attributes_time").set("#{hour}:#{minutes}AM")
         click_button "登録"
 
         expect(find("#overlay-commit-message", visible: true)).to be_visible
